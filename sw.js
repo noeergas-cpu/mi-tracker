1f533181a0e2ce52a38c6d5249cea6c7149ef093
noeergas-cpu <noeergas@gmail.com>
2026-07-09 12:29:01 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Service worker: permite instalar la app y usarla sin internet
+const CACHE = 'mi-tracker-v1';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
